b71d06d028a40ba0fc70a771f88a9bf5a2a8bf4c
Xin Xu <xinx1@andrew.cmu.edu>
2017-11-10 13:29:24 -0500

    add opencv

--- main.cpp
+++ main.cpp
@@ -2,19 +2,41 @@
 // Created by Xin Xu on 11/9/17.
 //
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
+#include <string>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "gaussian.h"
 
+using namespace cv;
 using namespace std;
 
-int main() {
+void display(Mat& im);
 
-    cout << "Create gaussian filter" << endl;
-    float* gaussian = createGaussianFilter(3, 3, 0.5);
-    printGaussianFilter(gaussian, 3, 3);
+int main(int argc, char** argv) {
 
+//    cout << "Create gaussian filter" << endl;
+//    float* gaussian = createGaussianFilter(3, 3, 0.5);
+//    printGaussianFilter(gaussian, 3, 3);
+
+    String im1_name( "../data/incline_L.png");
+    Mat im1_color = imread(im1_name, IMREAD_COLOR);
+    Mat im1_gray;
+    cvtColor(im1_color,im1_gray,CV_RGB2GRAY);
+
+
+    display(im1_gray);
     return 0;
+}
+
+void display(Mat& im) {
+    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
+    imshow( "Display window", im);                // Show our image inside it.
+    waitKey(0);
 }
