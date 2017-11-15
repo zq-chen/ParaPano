97d767378a98382cdeac8ec039fb3335f988b6c0
Xin Xu <xinx1@andrew.cmu.edu>
2017-11-15 00:47:22 -0500

    used opencv findHomography and finished stitcher

---
+++ stitcher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Xin Xu on 11/15/17.
+//
+
+#include "stitcher.h"
---
+++ stitcher.h
@@ -0,0 +1,8 @@
+//
+// Created by Xin Xu on 11/15/17.
+//
+
+#ifndef PARAPANO_STITCHER_H
+#define PARAPANO_STITCHER_H
+
+#endif //PARAPANO_STITCHER_H
